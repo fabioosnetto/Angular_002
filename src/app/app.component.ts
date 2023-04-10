ab22b704eb73b6517c928130e757e38ce9723273
fabioosnetto <fabioorione8@gmail.com>
2023-04-10 15:57:36 -0300

    form and ng-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Angular_002';
+
+  user: User = {name: '', gender: ''};
+  show: boolean = false;
+  
+  ngOnInit(){
+    
+  }
+
+  showButton(){
+    this.show = !this.show;
+  }
+
+  logthat(){
+    console.log(`User: ${this.user.name} - Gender: ${this.user.gender}`);
+  }
 }
